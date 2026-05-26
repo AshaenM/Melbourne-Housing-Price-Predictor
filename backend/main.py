9f9e8d016bb711aa20800b20b2dd591de2b6eb39
AshaenM <ashaenmanuel41@gmail.com>
2026-05-26 13:10:00 +1000

    Add requirements.txt and model files

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # URL of React application
-    allow_credentials=True,
+    allow_origins=["*"], # URL of React application
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
